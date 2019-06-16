79e2273f403d641377cf94d02280e277d5fae5b5
Trevahr Robertson <trevahr@yahoo.com>
2019-06-16 23:14:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ end
 def get_english_meaning(file, japanese_emoticon)
   
   library = load_library(file)
-  meaning = library["get_meaning"].fetch(japanese_emoticon)
+  meaning = library["get_meaning"].fetch(japanese_emoticon, "Sorry, that emoticon was not found")
   meaning
   
 end
